db171c82440f68bc2a5ab0b558a40c95f9cd128c
Ciro Dourado <>
2021-04-15 18:24:38 -0300

    criando a classe arvore avl

---
+++ test/ArvoreAVL.cs
@@ -0,0 +1,15 @@
+using System;
+
+class ArvoreAVL: ArvoreBinaria {
+	// atributos
+	private ArvoreBinaria arvore;
+
+
+
+	// métodos abertos
+	public ArvoreAVL() {
+		this.arvore = new ArvoreBinaria();
+	} // fim construtor padrão
+
+
+} // fim da classe ArvoreAVL
