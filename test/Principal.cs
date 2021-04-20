4610ece8336d3ecbeeecc306b9ffec6c706784aa
Ciro Dourado <>
2021-04-20 14:10:27 -0300

    codigo para testar se rotação funciona

--- test/Principal.cs
+++ test/Principal.cs
@@ -6,6 +6,16 @@ class Principal {
 	static void Main() {
 		var contatos = new ArvoreAVL();
 
+		contatos.Inserir(new Pessoa("Cais", "1"));
+		Console.WriteLine("{0}",
+			contatos.Raiz().ID()
+		); // a raiz inicialmente é Cais
+
+		contatos.Inserir(new Pessoa("Deno", "2"));
+		contatos.Inserir(new Pessoa("Elmo", "3"));
+		Console.WriteLine("{0}",
+			contatos.Raiz().ID()
+		); // a nova raiz deve ser Deno
 
 	} // fim da Main
 
